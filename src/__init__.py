9438c904e5a4a00f8bab903e6d58973b240ac957
veenayaraddi <veenashankar.yaraddi@hcltech.com>
2025-03-13 10:00:04 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+# You can add initialization code here
+# For example, exposing a function from this module:
+from .module_name import some_function
+
+__version__ = "1.0.0"  # Version of your package
+
+# Other imports or package-level variables can go here
